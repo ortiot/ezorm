37a08f7f24815f436309eb738f9041038a6b7221
guishan <guishan@dian.so>
2019-05-21 10:08:14 +0800

    update the maxopen

--- sql_factory_impl.go
+++ sql_factory_impl.go
@@ -1,14 +1,15 @@
 package ezorm
 
 import (
-	"encoding/xml"
-	"io/ioutil"
-	"text/template"
-	"strings"
-	"errors"
-	"reflect"
 	"database/sql"
+	"encoding/xml"
+	"errors"
 	"fmt"
+	"io/ioutil"
+	"reflect"
+	"strings"
+	"text/template"
+	"time"
 )
 
 /**
@@ -21,23 +22,23 @@ import (
 
 type sqlFactoryImpl struct {
 	tplRepositories map[string]sqlRepository
-	dataSourceName string
-	driverName string
-	dbName string
+	dataSourceName  string
+	driverName      string
+	dbName          string
 }
 
-type mapperXML struct{
-	Namespace     string `xml:"namespace,attr"`
-	Children []templateElem `xml:",any"`
+type mapperXML struct {
+	Namespace string         `xml:"namespace,attr"`
+	Children  []templateElem `xml:",any"`
 }
 
-type templateElem struct{
+type templateElem struct {
 	XMLName xml.Name
-	Id string `xml:"id,attr"`
-	Sql string `xml:",chardata"`
+	Id      string `xml:"id,attr"`
+	Sql     string `xml:",chardata"`
 }
 
-func(fac *sqlFactoryImpl) build(dbName,driverName,dataSourceName,mapperPath string) error{
+func (fac *sqlFactoryImpl) build(dbName, driverName, dataSourceName, mapperPath string) error {
 	fac.tplRepositories = make(map[string]sqlRepository)
 	fac.dbName = dbName
 	fac.dataSourceName = dataSourceName
@@ -49,15 +50,15 @@ func(fac *sqlFactoryImpl) build(dbName,driverName,dataSourceName,mapperPath stri
 	}
 
 	for _, f := range files {
-		if f.IsDir(){
+		if f.IsDir() {
 			continue
 		}
 
-		filePath := mapperPath+"/"+f.Name()
+		filePath := mapperPath + "/" + f.Name()
 
-		err = fac.parseMapperFile(driverName,filePath)
+		err = fac.parseMapperFile(driverName, filePath)
 
-		if err!=nil{
+		if err != nil {
 			return err
 		}
 	}
@@ -65,7 +66,7 @@ func(fac *sqlFactoryImpl) build(dbName,driverName,dataSourceName,mapperPath stri
 	return nil
 }
 
-func(fac *sqlFactoryImpl) parseMapperFile(driverName,mapperFilePath string)error{
+func (fac *sqlFactoryImpl) parseMapperFile(driverName, mapperFilePath string) error {
 	content, err := ioutil.ReadFile(mapperFilePath)
 	if err != nil {
 		return err
@@ -77,22 +78,22 @@ func(fac *sqlFactoryImpl) parseMapperFile(driverName,mapperFilePath string)error
 		return err
 	}
 
-	rep,exist := fac.tplRepositories[mapper.Namespace]
+	rep, exist := fac.tplRepositories[mapper.Namespace]
 
-	if !exist{
+	if !exist {
 		rep = newSqlRepository()
 		fac.tplRepositories[mapper.Namespace] = rep
 	}
 
-	for _,m:=range mapper.Children{
-		tpl,err := template.New(m.Id).Parse(m.Sql)
-		if err!=nil{
+	for _, m := range mapper.Children {
+		tpl, err := template.New(m.Id).Parse(m.Sql)
+		if err != nil {
 			return err
 		}
 
-		err = rep.addStatement(m.Id,strings.ToLower(m.XMLName.Local),driverName,tpl)
+		err = rep.addStatement(m.Id, strings.ToLower(m.XMLName.Local), driverName, tpl)
 
-		if err!=nil{
+		if err != nil {
 			return err
 		}
 	}
@@ -100,130 +101,132 @@ func(fac *sqlFactoryImpl) parseMapperFile(driverName,mapperFilePath string)error
 	return nil
 }
 
-func(fac *sqlFactoryImpl) QueryRows(namespace,id string,param interface{},rows interface{})error{
-	if fac.tplRepositories == nil{
-		return fmt.Errorf("SQL FACTORY %v NOT FOUND",fac.dbName)
+func (fac *sqlFactoryImpl) QueryRows(namespace, id string, param interface{}, rows interface{}) error {
+	if fac.tplRepositories == nil {
+		return fmt.Errorf("SQL FACTORY %v NOT FOUND", fac.dbName)
 	}
 
-	rep,exist := fac.tplRepositories[namespace]
+	rep, exist := fac.tplRepositories[namespace]
 
-	if !exist{
+	if !exist {
 		return errors.New("NAMESPACE NOT FOUND")
 	}
 
 	rowsPtr := reflect.ValueOf(rows)
 
-	if rowsPtr.Kind()!=reflect.Ptr{
+	if rowsPtr.Kind() != reflect.Ptr {
 		return errors.New("ROWS ONLY SUPPORT POINT TYPE")
 	}
 
 	rowsVal := reflect.Indirect(rowsPtr)
 
-	if rowsVal.Kind()!=reflect.Slice{
+	if rowsVal.Kind() != reflect.Slice {
 		return errors.New("THE VALUE OF ROWS ONLY SUPPORT SLICE TYPE")
 	}
 
-	sql,args,err := rep.build("select",id,param)
+	sql, args, err := rep.build("select", id, param)
 
-	if err!=nil{
+	if err != nil {
 		return err
 	}
 
-	db,err := fac.GetDB()
+	db, err := fac.GetDB()
+	defer db.Close()
 
-	if err!=nil{
+	if err != nil {
 		return err
 	}
 
-	rs,err := db.Query(sql,args...)
+	rs, err := db.Query(sql, args...)
 
-	if err!=nil{
+	if err != nil {
 		return err
 	}
 
 	defer rs.Close()
 
-	colsMap,scanList,err := getColsInfo(rs)
+	colsMap, scanList, err := getColsInfo(rs)
 
-	if err!=nil{
+	if err != nil {
 		return err
 	}
 
 	rowType := rowsVal.Type().Elem()
 
-	for rs.Next(){
+	for rs.Next() {
 		item := reflect.New(rowType).Elem()
 
 		err = rs.Scan(scanList...)
 
-		if err!=nil{
+		if err != nil {
 			return err
 		}
 
-		setRowVal(rowType,item,colsMap,scanList)
+		setRowVal(rowType, item, colsMap, scanList)
 
-		rowsVal.Set(reflect.Append(rowsVal,item))
+		rowsVal.Set(reflect.Append(rowsVal, item))
 	}
 
 	return nil
 }
 
-func(fac *sqlFactoryImpl) QueryRow(namespace,id string,param interface{},row interface{})error{
-	if fac.tplRepositories == nil{
-		return fmt.Errorf("SQL FACTORY %v NOT FOUND",fac.dbName)
+func (fac *sqlFactoryImpl) QueryRow(namespace, id string, param interface{}, row interface{}) error {
+	if fac.tplRepositories == nil {
+		return fmt.Errorf("SQL FACTORY %v NOT FOUND", fac.dbName)
 	}
 
-	rep,exist := fac.tplRepositories[namespace]
+	rep, exist := fac.tplRepositories[namespace]
 
-	if !exist{
+	if !exist {
 		return errors.New("NAMESPACE NOT FOUND")
 	}
 
 	rowsPtr := reflect.ValueOf(row)
 
-	if rowsPtr.Kind()!=reflect.Ptr{
+	if rowsPtr.Kind() != reflect.Ptr {
 		return errors.New("ROW ONLY SUPPORT POINT TYPE")
 	}
 
-	sql,args,err := rep.build("select",id,param)
+	sql, args, err := rep.build("select", id, param)
 
-	if err!=nil{
+	if err != nil {
 		return err
 	}
 
-	db,err := fac.GetDB()
+	db, err := fac.GetDB()
+	defer db.Close()
 
-	if err!=nil{
+	if err != nil {
 		return err
 	}
 
-	rs,err := db.Query(sql,args...)
+	rs, err := db.Query(sql, args...)
 
-	if err!=nil{
+	if err != nil {
 		return err
 	}
 
 	defer rs.Close()
 
-	colsMap,scanList,err := getColsInfo(rs)
+	colsMap, scanList, err := getColsInfo(rs)
 
-	if err!=nil{
+	if err != nil {
 		return err
 	}
 
 	rowVal := reflect.Indirect(rowsPtr)
 	rowType := rowVal.Type()
 
-	if rs.Next(){
+	if rs.Next() {
 		item := reflect.New(rowType).Elem()
 
 		err = rs.Scan(scanList...)
 
-		if err!=nil{
+		if err != nil {
 			return err
 		}
 
-		setRowVal(rowType,item,colsMap,scanList)
+		setRowVal(rowType, item, colsMap, scanList)
 
 		rowVal.Set(item)
 	}
@@ -231,14 +234,14 @@ func(fac *sqlFactoryImpl) QueryRow(namespace,id string,param interface{},row int
 	return nil
 }
 
-func(fac *sqlFactoryImpl)Query(param interface{}) error{
-	if fac.tplRepositories == nil{
-		return fmt.Errorf("SQL FACTORY %v NOT FOUND",fac.dbName)
+func (fac *sqlFactoryImpl) Query(param interface{}) error {
+	if fac.tplRepositories == nil {
+		return fmt.Errorf("SQL FACTORY %v NOT FOUND", fac.dbName)
 	}
 
 	paramPtr := reflect.ValueOf(param)
 
-	if paramPtr.Kind()!=reflect.Ptr{
+	if paramPtr.Kind() != reflect.Ptr {
 		return errors.New("QUERY ONLY SUPPORT POINT PARAM")
 	}
 
@@ -246,11 +249,11 @@ func(fac *sqlFactoryImpl)Query(param interface{}) error{
 	paramType := paramVal.Type()
 	ns := paramType.PkgPath()
 
-	for i:=0;i<paramType.NumField();i++{
+	for i := 0; i < paramType.NumField(); i++ {
 		f := paramType.Field(i)
 		stmtId := f.Tag.Get("stmt")
 
-		if stmtId==""{
+		if stmtId == "" {
 			continue
 		}
 
@@ -259,12 +262,12 @@ func(fac *sqlFactoryImpl)Query(param interface{}) error{
 		fVal := paramVal.Field(i).Addr().Interface()
 
 		if f.Type.Kind() == reflect.Slice {
-			err = fac.QueryRows(ns,stmtId,param,fVal)
-		}else {
-			err = fac.QueryRow(ns,stmtId,param,fVal)
+			err = fac.QueryRows(ns, stmtId, param, fVal)
+		} else {
+			err = fac.QueryRow(ns, stmtId, param, fVal)
 		}
 
-		if err!=nil{
+		if err != nil {
 			return err
 		}
 	}
@@ -272,61 +275,64 @@ func(fac *sqlFactoryImpl)Query(param interface{}) error{
 	return nil
 }
 
-func(fac *sqlFactoryImpl) Update(namespace string, id string, param interface{})(rowsAffected int64,err error){
-	return fac.Execute("update",namespace,id,param)
+func (fac *sqlFactoryImpl) Update(namespace string, id string, param interface{}) (rowsAffected int64, err error) {
+	return fac.Execute("update", namespace, id, param)
 }
 
-func(fac *sqlFactoryImpl) Insert(namespace string, id string, param interface{})(lastInsertId int64,err error){
-	return fac.Execute("insert",namespace,id,param)
+func (fac *sqlFactoryImpl) Insert(namespace string, id string, param interface{}) (lastInsertId int64, err error) {
+	return fac.Execute("insert", namespace, id, param)
 }
 
-func(fac *sqlFactoryImpl) Execute(sqlType, namespace string, id string, param interface{})(cnt int64,err error){
-	if fac.tplRepositories == nil{
-		err = fmt.Errorf("SQL FACTORY %v NOT FOUND",fac.dbName)
+func (fac *sqlFactoryImpl) Execute(sqlType, namespace string, id string, param interface{}) (cnt int64, err error) {
+	if fac.tplRepositories == nil {
+		err = fmt.Errorf("SQL FACTORY %v NOT FOUND", fac.dbName)
 		return
 	}
 
-	rep,exist := fac.tplRepositories[namespace]
+	rep, exist := fac.tplRepositories[namespace]
 
-	if !exist{
+	if !exist {
 		err = errors.New("NAMESPACE NOT FOUND")
 
 		return
 	}
 
-	sql,args,err := rep.build(sqlType,id,param)
+	sql, args, err := rep.build(sqlType, id, param)
 
-	if err!=nil{
+	if err != nil {
 		return
 	}
 
-	db,err := fac.GetDB()
+	db, err := fac.GetDB()
 
-	if err!=nil{
+	if err != nil {
 		return
 	}
 
 	defer db.Close()
 
-	rs,err := db.Exec(sql,args...)
+	rs, err := db.Exec(sql, args...)
 
-	if err!=nil{
+	if err != nil {
 		return
 	}
 
-	if sqlType=="insert"{
-		cnt,err = rs.LastInsertId()
-	}else{
-		cnt,err = rs.RowsAffected()
+	if sqlType == "insert" {
+		cnt, err = rs.LastInsertId()
+	} else {
+		cnt, err = rs.RowsAffected()
 	}
 
 	return
 }
 
-func(fac *sqlFactoryImpl)GetDB()(db *sql.DB, err error){
-	if fac.driverName==""||fac.dataSourceName==""{
-		return nil,fmt.Errorf("SQL FACTORY %v NOT FOUND",fac.dbName)
+func (fac *sqlFactoryImpl) GetDB() (db *sql.DB, err error) {
+	if fac.driverName == "" || fac.dataSourceName == "" {
+		return nil, fmt.Errorf("SQL FACTORY %v NOT FOUND", fac.dbName)
 	}
-
-	return sql.Open(fac.driverName, fac.dataSourceName)
+	db, err = sql.Open(fac.driverName, fac.dataSourceName)
+	db.SetMaxOpenConns(10)
+	db.SetMaxIdleConns(10)
+	db.SetConnMaxLifetime(10 * time.Minute)
+	return
 }
